b557794ee562aa174919c315861c443112e2a9db
Tony <krixen.dev@gmail.com>
2024-02-05 20:56:58 -0300

    agrega crear campos en el formulario y calcular edades

--- tarea1-clase6.js
+++ tarea1-clase6.js
@@ -6,4 +6,64 @@ Al hacer click en "calcular", mostrar en un elemento pre-existente la mayor edad
 Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuevamente, borrando los inputs ya creados (investigar cómo en MDN).
 */
 
+const cantidadIntegrantes = Number(prompt('Cuántos integrantes hay en tu grupo familiar?'));
+const $formulario = document.querySelector('form');
+const calcular = document.querySelector("#calcular");
 
+
+agregarCamposFormulario(cantidadIntegrantes, $formulario);
+
+calcular.onclick = function () {
+  const datos = $formulario.querySelectorAll("input");
+  let edades = []
+  for (const edad of datos) {
+    edades.push(Number(edad.value));
+  }
+
+  console.log(calcularPromedio(edades));
+  console.log(encontrarNumeroMasGrande(edades));
+  console.log(encontrarNumeroMasChico(edades));
+  return false;
+}
+
+function agregarCamposFormulario(cantidad, $formulario) {
+  for (let i = 1; i < cantidad + 1; i++) {
+    const label = document.createElement("label");
+    label.textContent = `integrante: ${i}`;
+    $formulario.appendChild(label)
+    const input = document.createElement("input");
+    $formulario.appendChild(input);
+  }
+  return $formulario
+}
+
+function calcularPromedio(numeros) {
+  let sumaNumeros = 0;
+  const cantidadNumeros = numeros.length;
+  for (let i = 0; i < cantidadNumeros; i++) {
+    sumaNumeros += Number(numeros[i]);
+  }
+  return sumaNumeros / cantidadNumeros;
+}
+
+function encontrarNumeroMasGrande(numeros) {
+  let numeroMasGrandeEncontrado = numeros[0];
+  for (let i = 0; i < numeros.length; i++) {
+    let numeroMayorActual = numeros[i];
+    if (numeroMayorActual > numeroMasGrandeEncontrado) {
+      numeroMasGrandeEncontrado = numeroMayorActual;
+    }
+  }
+  return numeroMasGrandeEncontrado;
+}
+
+function encontrarNumeroMasChico(numeros) {
+  let numeroMasChicoEncontrado = numeros[0];
+  for (let i = 0; i < numeros.length; i++) {
+    let numeroMenorActual = numeros[i];
+    if (numeroMenorActual < numeroMasChicoEncontrado) {
+      numeroMasChicoEncontrado = numeroMenorActual;
+    }
+  }
+  return numeroMasChicoEncontrado;
+}
